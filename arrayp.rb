c7a19d555c8acf8ac6cf01c9cd4ad4f58077d346
AnyaWall <anyawall@gmail.com>
2018-07-25 15:24:00 +0000

    automatically backed up by learn

--- arrayp.rb
+++ arrayp.rb
@@ -5,9 +5,10 @@ tvShow[2] = "Pokemon"
 tvShow[3] = "The Office"
 tvShow[4] = "Full House"
 
-puts "#{tvShows[2]}"
+puts "#{tvShow[2]}"
 
 # I want to take an array with the above values
 # I want to loop through the entire array
 # I want to puts out the name of all the shows in the array currently
 
+index = 0
