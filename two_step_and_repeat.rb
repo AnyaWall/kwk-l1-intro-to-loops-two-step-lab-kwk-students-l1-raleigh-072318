b8a7efed0027b3dd0e72a3832a997472c11808c8
AnyaWall <anyawall@gmail.com>
2018-07-25 14:30:34 +0000

    Done.

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -15,3 +15,32 @@ def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
 end
+
+
+loop do
+  puts "right foot back"
+  sleep(0.5)
+  puts "lefts foot back"
+  sleep(0.5)
+  puts "stop"
+  sleep(1)
+  puts "right foot steps right and back"
+  sleep(0.5)
+  puts "left foot crosses over right"
+  sleep(0.5)
+  puts "right foot steps right"
+  sleep(0.5)
+  puts "turn"
+  sleep(1)
+ end
+end
+
+def break_dance
+  steps = 0
+loop do
+  steps += 1 
+  puts steps
+if steps % 2
+  
+
+
